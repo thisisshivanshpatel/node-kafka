9e9b08d66c45da5814ce5403c8a2cbdffe0ed1ba
shivansh patel <shivanshpatel.1999@gmail.com>
2024-06-09 19:01:09 +0530

    kafka app initialized

---
+++ producer.js
@@ -0,0 +1,39 @@
+const { kafka } = require("./client");
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+async function init() {
+  const producer = kafka.producer();
+
+  console.log("connecting Producer");
+
+  await producer.connect();
+  console.log("Producer connected successfully");
+
+  rl.setPrompt("> ");
+  rl.prompt();
+
+  rl.on("line", async function (line) {
+    const [riderName, location] = line.split(" ");
+
+    await producer.send({
+      topic: "rider-updates",
+      messages: [
+        {
+          partition: location.toLowerCase() === "north" ? 0 : 1,
+          key: "location-update",
+          value: JSON.stringify({ name: riderName, location }),
+        },
+      ],
+    });
+  }).on("close", async () => {
+    console.log("disconnecting......");
+    await producer.disconnect();
+  });
+}
+
+init();
